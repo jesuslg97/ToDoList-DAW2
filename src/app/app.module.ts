faf2902be120eb399c884087672db7c40ddd6c38
jesus <jelopezgonzalez@gmail.com>
2021-05-02 12:56:22 +0100

    firebase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestore } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig)
   ],
-  providers: [],
+  providers: [AngularFirestore],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyC-FNSWIeOeWjYu10XM7oLkiUAVgi2f8VY",
+    authDomain: "todolist-daw2.firebaseapp.com",
+    projectId: "todolist-daw2",
+    storageBucket: "todolist-daw2.appspot.com",
+    messagingSenderId: "603779958481",
+    appId: "1:603779958481:web:8e7e7fa23c04e3849f67f0"
+  }
 };
 
 /*
