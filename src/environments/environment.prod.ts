dee0801e042be8e29886cadd6d3afb4dde9f96da
jesus <jelopezgonzalez@gmail.com>
2021-05-28 20:24:14 +0100

    deploy

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyC-FNSWIeOeWjYu10XM7oLkiUAVgi2f8VY",
+    authDomain: "todolist-daw2.firebaseapp.com",
+    projectId: "todolist-daw2",
+    storageBucket: "todolist-daw2.appspot.com",
+    messagingSenderId: "603779958481",
+    appId: "1:603779958481:web:8e7e7fa23c04e3849f67f0"
+  }
 };
